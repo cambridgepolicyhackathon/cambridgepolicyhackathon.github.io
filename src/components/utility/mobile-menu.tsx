46362c111edee3f8e351ba46ea51d83d2a0d3e96
xlongfu <153302396+xlongfu@users.noreply.github.com>
2026-02-10 10:45:24 +0000

    Update mobile-menu.tsx

--- src/components/utility/mobile-menu.tsx
+++ src/components/utility/mobile-menu.tsx
@@ -28,40 +28,44 @@ export default function MobileMenu({
 
   return (
     <Transition show={openMenu} as={Fragment}>
-      <Dialog as="div" className="z-50" onClose={setOpenMenu}>
-        <div className="fixed inset-0 flex items-center justify-center">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 bottom-full"
-            enterTo="opacity-100 bottom-[15%]"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 bottom-[15%]"
-            leaveTo="opacity-0 bottom-full"
-          >
-            <Dialog.Panel className="pointer-events-none absolute flex min-h-[85%] w-full flex-col items-center justify-center overflow-y-auto rounded-b-2xl border-2 border-accent/20 bg-background px-6 py-8 text-accent shadow-lg shadow-accent/10 md:px-10 md:py-16">
-              <div className="pointer-events-auto flex flex-col items-center gap-6 text-center">
-                {routes.map((link, i) => (
-                  <button
-                    key={i}
-                    className="group relative py-2 text-3xl font-medium"
-                    onClick={() => handleClick(link.href)}
-                  >
-                    <span
-                      className={classNames(
-                        pathName === link.href ? "w-full" : "w-0",
-                        "absolute -bottom-1 left-0 h-1 rounded-lg bg-accent transition-[width] duration-300 group-hover:w-full",
-                      )}
-                    ></span>
-                    {link.title}
-                  </button>
-                ))}
-                <ThemeSwitch setClose={setOpenMenu} />
-              </div>
-              <div className="absolute bottom-0 py-6">©{new Date().getFullYear()} Cambridge Policy Hackathon</div>
-            </Dialog.Panel>
-          </Transition.Child>
-        </div>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-[9999] flex items-center justify-center"
+        onClose={setOpenMenu}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 bottom-full"
+          enterTo="opacity-100 bottom-[15%]"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 bottom-[15%]"
+          leaveTo="opacity-0 bottom-full"
+        >
+          <Dialog.Panel className="pointer-events-none relative flex min-h-[85%] w-full flex-col items-center justify-center overflow-y-auto rounded-b-2xl border-2 border-accent/20 bg-background px-6 py-8 text-accent shadow-lg shadow-accent/10 md:px-10 md:py-16">
+            <div className="pointer-events-auto flex flex-col items-center gap-6 text-center">
+              {routes.map((link, i) => (
+                <button
+                  key={i}
+                  className="group relative py-2 text-3xl font-medium"
+                  onClick={() => handleClick(link.href)}
+                >
+                  <span
+                    className={classNames(
+                      pathName === link.href ? "w-full" : "w-0",
+                      "absolute -bottom-1 left-0 h-1 rounded-lg bg-accent transition-[width] duration-300 group-hover:w-full",
+                    )}
+                  ></span>
+                  {link.title}
+                </button>
+              ))}
+              <ThemeSwitch setClose={setOpenMenu} />
+            </div>
+            <div className="absolute bottom-0 py-6">
+              ©{new Date().getFullYear()} Cambridge Policy Hackathon
+            </div>
+          </Dialog.Panel>
+        </Transition.Child>
       </Dialog>
     </Transition>
   );
